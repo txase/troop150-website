9dd0d4d74f6696f9f3aa7ec3d1fa6841853d8901
Chase Douglas <chasedouglas@gmail.com>
2024-09-12 02:03:09 +0000

    Decode HTML characters from calendar events
    And indent FullCalendar props, as they should have been originally.

--- src/layouts/components/FullCalendar.tsx
+++ src/layouts/components/FullCalendar.tsx
@@ -4,31 +4,36 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import iCalendarPlugin from '@fullcalendar/icalendar'
 import theme from '@/config/theme.json'
 
+// This function safely decodes HTML, avoiding potential XSS attacks
+const htmlDecode = (html: string) => new DOMParser()
+    .parseFromString(html, 'text/html')
+    .documentElement.textContent;
+
 const Calendar = ({}: {}) => {
   return (
     <FullCalendar
-    viewClassNames={"fullcalendar-view"}
-    plugins={[ dayGridPlugin, iCalendarPlugin ]}
-    initialView="dayGridMonth"
-    eventSources={[
-      {
-        id: 'Troop 150',
-        url: '/calendar/150',
-        format: 'ics',
-        color: theme.colors.bsa.blue
-      },
-      {
-        id: 'Troop 5150',
-        url: '/calendar/5150',
-        format: 'ics',
-        color: theme.colors.bsa.yellow
-      }
-    ]}
-    eventClick={info => {
-      info.jsEvent.preventDefault();
+      viewClassNames={"fullcalendar-view"}
+      plugins={[ dayGridPlugin, iCalendarPlugin ]}
+      initialView="dayGridMonth"
+      eventSources={[
+        {
+          id: 'Troop 150',
+          url: '/calendar/150',
+          format: 'ics',
+          color: theme.colors.bsa.blue
+        },
+        {
+          id: 'Troop 5150',
+          url: '/calendar/5150',
+          format: 'ics',
+          color: theme.colors.bsa.yellow
+        }
+      ]}
+      eventClick={info => {
+        info.jsEvent.preventDefault();
 
-      alert(`${info.event.source?.id} - ${info.event.title}\n\nLocation: ${info.event.extendedProps.location}\n\n${info.event.extendedProps.description}`);
-    }}
+        alert(`${info.event.source?.id} - ${htmlDecode(info.event.title)}\n\nLocation: ${htmlDecode(info.event.extendedProps.location)}\n\n${htmlDecode(info.event.extendedProps.description)}`);
+      }}
     />
   );
 };
